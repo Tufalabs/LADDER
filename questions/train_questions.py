991ada032a1c27cf1b90e29681be43eca6debfcf
Tamas Simond <tamassimonds@gmail.com>
2025-02-21 16:01:34 +1100

    all variants

---
+++ questions/train_questions.py
@@ -0,0 +1,16 @@
+# This file is auto-generated and contains the integrals that were answered incorrectly.
+
+TRAIN_QUESTIONS = [
+    'integrate((x-3)/(x**2+3), x)',
+    'integrate(cosh(x)**2, x)',
+    'integrate(exp(x)*sin(x), x)',
+    'integrate(x**2*exp(x), x)',
+    'integrate(exp(x)*cos(x), x)',
+    'integrate(x/(x**2 + 1), x)',
+    'integrate(sin(2*x)**2, x)',
+    'integrate(x**3/(1+x**4), x)',
+    'integrate(1/(1+exp(-x)), x)',
+    'integrate(1/(cosh(x)), x)',
+  
+    
+]
